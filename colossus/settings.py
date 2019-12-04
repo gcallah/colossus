9c0a8d51ec7adc972de87a66925228290f5e7c57
Yangyang Xu <xuyangyang1205@hotmail.com>
2019-12-04 13:29:23 -0500

    Removed unnecessary blank lines

--- colossus/settings.py
+++ colossus/settings.py
@@ -245,6 +245,4 @@ COLOSSUS_HTTPS_ONLY = config('COLOSSUS_HTTPS_ONLY', default=False, cast=bool)
 
 MAILGUN_API_KEY = config('MAILGUN_API_KEY', default='e13e3189d397370e47fd3c3069c3a32a-e470a504-62090771')
 
-
-
 MAILGUN_API_BASE_URL = config('MAILGUN_API_BASE_URL', default='')
