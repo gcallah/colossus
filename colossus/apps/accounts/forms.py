58a4b487862db641e3766acdabf6018d8426ca8b
djmahida <dev.mahida@nyu.edu>
2020-03-16 21:25:16 -0400

    Added comments in the forms.py file in the Accounts App.

--- colossus/apps/accounts/forms.py
+++ colossus/apps/accounts/forms.py
@@ -8,11 +8,26 @@ from .models import User
 
 
 class AdminUserCreationForm(UserCreationForm):
+    """
+    This is a class used for admin sign-up forms.
+    """
     class Meta:
+        """
+        The is the inner class to define form fields for admin sign-up forms.
+        """
         model = User
         fields = ('username', 'email',)
 
     def save(self, commit=True):
+        """
+        The function to add a new admin user to the user database.
+
+        Keyword arguments:
+        commit -- A boolean variable to decide if the change must be commit or not.
+
+        Returns:
+        user -- The object of new user added into the database.
+        """
         user = super().save(commit=False)
         user.is_staff = True
         user.is_superuser = True
@@ -22,6 +37,9 @@ class AdminUserCreationForm(UserCreationForm):
 
 
 class UserForm(forms.ModelForm):
+    """
+    This is a class used for user sign-up forms.
+    """
     TIMEZONE_CHOICES = (('', '---------'),) + tuple(map(lambda tz: (tz, tz), pytz.common_timezones))
 
     timezone = forms.ChoiceField(
@@ -31,5 +49,8 @@ class UserForm(forms.ModelForm):
     )
 
     class Meta:
+        """
+        The is the inner class to define form fields for user (non-admins) sign-up forms.
+        """
         model = User
         fields = ('first_name', 'last_name', 'email', 'timezone')
