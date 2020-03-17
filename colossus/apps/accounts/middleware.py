40450102b41220e4b935101d20eb3f16269274da
djmahida <dev.mahida@nyu.edu>
2020-03-16 23:56:21 -0400

    Added comments in middleware.py file in Accounts App.

--- colossus/apps/accounts/middleware.py
+++ colossus/apps/accounts/middleware.py
@@ -5,10 +5,25 @@ from pytz import UnknownTimeZoneError
 
 
 class UserTimezoneMiddleware:
+    """
+    This is a middleware class for user timezones.
+    """
     def __init__(self, get_response):
+        """
+        An init function to define self.get_response
+        """
         self.get_response = get_response
 
     def __call__(self, request):
+        """
+        A function to set the timezone of user and get response from the server.
+
+        Keyword arguments:
+        request -- The http request object
+
+        Returns:
+        response -- The http response object
+        """
         if request.user.is_authenticated:
             try:
                 timezone.activate(pytz.timezone(request.user.timezone))
