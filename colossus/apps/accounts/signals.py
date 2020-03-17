8a508413d962a22dfd66a6222cd81933f2f3819f
djmahida <dev.mahida@nyu.edu>
2020-03-16 23:19:21 -0400

    Added comments in view.py & signals.py files in the Accounts App.

--- colossus/apps/accounts/signals.py
+++ colossus/apps/accounts/signals.py
@@ -12,17 +12,44 @@ logger = logging.getLogger(__name__)
 
 @receiver(user_logged_in)
 def log_user_logged_in(sender, request, user, **kwargs):
+    """
+    A function to log an entry whenever a user successfully logs in.
+    This function gets called automatically whenever the login is successful.
+
+    Keyword arguments:
+    sender -- The class of the user that just logged in.
+    request -- The current HttpRequest instance.
+    user -- The user instance that just logged in.
+    """
     ip_address = get_client_ip(request)
     logger.info('User "%s" logged in with IP address "%s".' % (user, ip_address))
 
 
 @receiver(user_logged_out)
 def log_user_logged_out(sender, request, user, **kwargs):
+    """
+    A function to log an entry whenever a user successfully logs out.
+    This function gets called automatically whenever the logout is successful.
+
+    Keyword arguments:
+    sender -- The class of the user that just logged in.
+    request -- The current HttpRequest instance.
+    user -- The user instance that just logged in.
+    """
     ip_address = get_client_ip(request)
     logger.info('User "%s" logged out with IP address "%s".' % (user, ip_address))
 
 
 @receiver(user_login_failed)
 def log_user_login_failed(sender, credentials, request, **kwargs):
+    """
+    A function to log an entry whenever a user fails to login.
+    This function gets called automatically whenever the login is failed.
+
+    Keyword arguments:
+    sender -- The class of the user that just logged in.
+    request -- The current HttpRequest instance.
+    user -- The user instance that just logged in.
+    """
     ip_address = get_client_ip(request)
     logger.warning('Log in failed for credentials "%s" with IP address "%s".' % (credentials, ip_address))
--- colossus/apps/accounts/views.py
+++ colossus/apps/accounts/views.py
@@ -8,10 +8,16 @@ from .models import User
 
 
 class ProfileView(LoginRequiredMixin, UpdateView):
+    """
+    This is the class for displaying the user profile.
+    """
     model = User
     form_class = UserForm
     template_name = 'accounts/profile.html'
     success_url = reverse_lazy('profile')
 
     def get_object(self, queryset=None):
+        """
+        This getter function gets the user object.
+        """
         return self.request.user
