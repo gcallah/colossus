8e7e4848484fcbe4c53d3d47af2bd932ce8b7ef1
djmahida <dev.mahida@nyu.edu>
2020-03-16 23:43:40 -0400

    Added comments in models.py file in Accounts App.

--- colossus/apps/accounts/models.py
+++ colossus/apps/accounts/models.py
@@ -3,7 +3,13 @@ from django.db import models
 
 
 class User(AbstractUser):
+    """
+    This is the class to define the user model.
+    """
     timezone = models.CharField(max_length=50, blank=True)
 
     class Meta:
+        """
+        This is the meta class of class User used to define the table name in the database.
+        """
         db_table = 'auth_user'
