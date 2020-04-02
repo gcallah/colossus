f7c8b25e29c6c7cdc56bf203d889cc5e1c80d59e
Chintan Thakkar <cdt303@nyu.edu>
2020-04-02 02:24:09 -0400

    Added echo statements for tracking deployment progress

--- rebuild.sh
+++ rebuild.sh
@@ -2,12 +2,12 @@
 # Script to automate pulling of project updates, installation of
 # dependencies and reloading the application in production
 
-# Pull updates from Git repository
+echo "Fetching updates from the Git Repository.."
 git pull origin master
 # Activate virtual environment:
 source /home/colossus/.virtualenvs/colossus/bin/activate
 # Install requirements:
 pip install -r requirements/production.txt
-# Reloading the application
+echo "Now, rebooting the server.."
 API_TOKEN=$1 pa_reload_webapp.py colossus.pythonanywhere.com
 touch reboot
