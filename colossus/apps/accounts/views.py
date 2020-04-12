a03d8a5ec18a6265a7f20f8d291a9d4243a0e501
djmahida <dev.mahida@nyu.edu>
2020-04-12 05:11:34 -0400

    Changed code becuase there was no logging.

--- colossus/apps/accounts/views.py
+++ colossus/apps/accounts/views.py
@@ -133,27 +133,23 @@ def ssoLogin(request):
             logger.info("Printing Session data {} ".format(request.session.items()))
             logger.info("Relay state : {}".format(req['post_data']['RelayState']))
             logger.info("Self URL : {}".format(OneLogin_Saml2_Utils.get_self_url(req)))
-            try:
-                userEmail = auth.get_attributes("mail")
-                userName = auth.get_attributes("givenName")
-                logger.info("USER EMAIL {}".format(userEmail))
-                logger.info("USER NAME {}".format(userName))
 
-                form = UserForm(data={
-                                        'first_name': 'Test1',
-                                        'last_name': '',
-                                        'email': 'sample@sample.com',
-                                        'timezone': 'America/New_York'
-                })
-                user = form.save()
-                logger.info("USER USERNAME : {}".format(user.email))
-                login(request, user)
-                user = get_user_model()
-                logger.info("USER USERNAME user.get_username: {}".format(user.get_username()))
-                logger.info("Print USER {}".format(user))
-                logger.info("USER.USERNAME {}".format(user.username))
-            except Exception:
-                pass
+            userEmail = auth.get_attributes("mail")
+            userName = auth.get_attributes("givenName")
+            logger.info("USER EMAIL {}".format(userEmail))
+            logger.info("USER NAME {}".format(userName))
+
+            form = UserForm(data={
+                                    'first_name': 'Test1',
+                                    'last_name': '',
+                                    'email': 'sample@sample.com',
+                                    'timezone': 'America/New_York'
+            })
+            user = form.save()
+            logger.info("USER USERNAME : {}".format(user.email))
+            login(request, user)
+            user = get_user_model()
+
             if 'RelayState' in req['post_data']:
                 logger.info("Inside Relay State")
                 # if OneLogin_Saml2_Utils.get_self_url(req) != req['post_data']['RelayState']:
