74f84949e3c83cdded34691f4e10be17aea19e02
Chintan Thakkar <cdt303@nyu.edu>
2020-04-20 17:56:55 -0400

    Added a decorator

--- colossus/apps/accounts/views.py
+++ colossus/apps/accounts/views.py
@@ -96,7 +96,11 @@ class LoginView(View):
                 target_url = req['post_data']['next']
             else:
                 target_url = '/'
-            return HttpResponseRedirect(auth.login(return_to=target_url))
+
+            @method_decorator(ensure_csrf_cookie)
+            def initiate_login():
+                return HttpResponseRedirect(auth.login(return_to=target_url))
+            return initiate_login()
 
         elif 'sso2' in req['get_data']:
             return_to = OneLogin_Saml2_Utils.get_self_url(req) + reverse('attrs')
