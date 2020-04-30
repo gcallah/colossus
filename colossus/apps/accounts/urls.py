ef24ea3b431a6d5ea89ad02b5f7cba12c410751a
djmahida <dev.mahida@nyu.edu>
2020-04-29 22:42:12 -0400

    Revert "Revert "Revert "Removed password change URLs as we now support SAML login only"""
    This reverts commit 490a1ffb2c7dc06afb1701fbb869b6a2546634aa.

--- colossus/apps/accounts/urls.py
+++ colossus/apps/accounts/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     path('attrs/', views.attributes, name="attrs"),
     path('metadata/', views.metadata, name="metadata"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
